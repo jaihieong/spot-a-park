b497de6420d4e5460e0201a448fef4f166ecd48a
jaihieong <jaihieong88@gmail.com>
2020-10-07 01:16:22 -0400

    all pages with navbar

--- spot-a-park.js
+++ spot-a-park.js
@@ -1,8 +1,8 @@
 console.log("connected: book-a-field app.js");
 
-let element = document.getElementById("testingID");
+// let element = document.getElementById("testingID");
 
-element.addEventListener("click", function(){
-  //action here
-  console.log("testingID onclick working")
-});
+// element.addEventListener("click", function(){
+//   //action here
+//   console.log("testingID onclick working")
+// });
